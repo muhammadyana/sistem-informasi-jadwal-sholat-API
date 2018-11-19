5f628b03e589418f86d56187f66ba74db47fca26
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-20 00:05:32 +0700

    :tada: Initial Commit :rocket:

---
+++ README.md
@@ -0,0 +1,12 @@
+# Siforlat Backend use node.js
+#### Instalation
+``` npm install ```
+``` npm install express —-save```
+``` npm install babel-cli --save```
+``` npm install babel-preset-es2015 --save ```
+
+### Run Server
+``` npm run start ```
+
+
+
---
+++ app.js
@@ -0,0 +1,45 @@
+// import express from 'express';
+import bodyParser from 'body-parser';
+var express       = require('express');
+var moment        = require('moment');
+var PrayTimes     = require('./PrayTimes');
+// Set up the express app
+const app = express();
+// console.log(`======= praytimes ${JSON.stringify(PrayTimes)}`);
+var date = new Date();
+var getSchedule = function(when){
+  console.log(`========= Date today ${moment().toDate()}`)
+  PrayTimes.setMethod('Karachi');
+  var toDay = moment();
+  if (when == 'tomorrow') {
+    date = toDay.add('1', 'days');
+  } else {
+    date = toDay;
+  }
+  
+  var lat = -6.300017; 
+  var lon = 106.670173;
+  return PrayTimes.getTimes(date.toDate(), [lat, lon], +7)
+};
+
+app.get('/api/v1/prayTimes', (req, res) => {
+  PrayTimes.setMethod('Karachi');
+  var toDay = moment();
+  // var lat = -6.300017; 
+  var lat = req.query.latitude; 
+  // var lon = 106.670173;
+  var lon = req.query.longitude;
+  res.status(200).send({
+    success: 'true',
+    schedule: PrayTimes.getTimes(toDay.toDate(), [lat, lon], +7)
+    // scedule: getSchedule()
+  })
+});
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
+
+// app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
