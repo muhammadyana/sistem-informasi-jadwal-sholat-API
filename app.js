296d326706ad625b4040ea9bec3ba6f036db0984
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-20 23:02:02 +0700

    :ambulance: fixed error in heroku :tada:

--- app.js
+++ app.js
@@ -36,11 +36,12 @@ app.get('/api/v1/prayTimes', (req, res) => {
     // schedule: singleSchedule
   })
 });
-const PORT = 5000;
 
-app.listen(PORT, () => {
-  console.log(`server running on port ${PORT}`)
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log("App is running on port " + port);
 });
+
 
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
