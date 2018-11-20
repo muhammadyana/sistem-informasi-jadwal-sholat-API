b2c7907f0433530903c507dff9af392ad81fb417
Muhammad Yana Mulyana <muhammadyanaa@gmail.com>
2018-11-20 17:13:59 +0700

    :abulance: fixed invalid schedule praytimes :tada:

--- app.js
+++ app.js
@@ -9,30 +9,31 @@ const app = express();
 // const currentMoment = moment().subtract(3, 'days');
 // const endMoment = moment().add(1, 'days');
 // console.log(`date after ${moment().add(30, 'days').format('YYYY-MM-DD')}`);
-const endDate = moment().add(30, 'days');
-const currentDate = moment();
-var praytimesSchedule = []
-// console.log(`curren date = ${currentDate.format('YYYY-MM-DD')}`);
-while (currentDate.isBefore(endDate, 'day')){
-  console.log(`Loop iteration up ${currentDate.format('YYYY-MM-DD')}`);
-  var loopDay = currentDate.format('YYYY-MM-DD');
-  var praytimesLoop = PrayTimes.getTimes(loopDay, [-6.300017, 106.670173], +7);
-  praytimesSchedule.push(praytimesLoop);
-  // currentDate.add(1, 'days');
-  currentDate.add(1, 'days');
-}
-
+  
 app.get('/api/v1/prayTimes', (req, res) => {
+  const endDate = moment().add(req.query.duration, 'days');
+  const currentDate = moment();
+  var praytimesSchedule = []
   PrayTimes.setMethod('Karachi');
-  var toDay = moment();
-  // var lat = -6.300017; 
   var lat = req.query.latitude; 
-  // var lon = 106.670173;
   var lon = req.query.longitude;
+  // console.log(`curren date = ${currentDate.format('YYYY-MM-DD')}`);
+  while (currentDate.isBefore(endDate, 'day')){
+    // console.log(`Loop iteration up ${currentDate.format('YYYY-MM-DD')}`);
+    var loopDay = currentDate.format('YYYY-MM-DD');
+    var praytimesLoop = PrayTimes.getTimes(currentDate.toDate(), [lat, lon], +7);
+    praytimesLoop.date = loopDay;
+    praytimesSchedule.push(praytimesLoop);
+    // currentDate.add(1, 'days');
+    currentDate.add(1, 'days');
+  }
+  var toDay = moment();
+  var singleSchedule = PrayTimes.getTimes(toDay.toDate(), [lat, lon], +7);
+  singleSchedule.date = toDay.format('YYYY-MM-DD');
   res.status(200).send({
     success: 'true',
     schedules: praytimesSchedule,
-    schedule: PrayTimes.getTimes(toDay.toDate(), [lat, lon], +7)
+    schedule: singleSchedule
     // scedule: getSchedule()
   })
 });
